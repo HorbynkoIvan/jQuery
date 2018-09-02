75e74d2456ca40947ce9182d073c66d9a82f8dfc
Ivan <horbynko.i@gmail.com>
2018-09-02 10:57:03 +0300

    methods html, text, width, height, location.reload

---
+++ Tutorials/2/script.js
@@ -0,0 +1,24 @@
+$(function () {
+    $('h1').on('click', function () {
+        $(this).css('color', 'green')
+    });
+
+    /*method html() using for html tags*/
+    $("#div1").html('<a href="example.html">Link</a><b>hello</b>');
+    /*method ttext() using for text*/
+    $("#div2").text('<a href="example.html">Link</a><b>hello</b>');
+
+    $('.btn_start').on('click', function () {
+        $('.message')
+            .html("Its message")
+            .css('color', 'orange')
+            .parent()
+            .css('background', 'red')
+            .width(100)
+            .height(50)
+    });
+    $('.btn_reset').on('click', function () {
+        /*use that method to reload page*/
+        location.reload();
+    })
+});

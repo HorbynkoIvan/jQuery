1e94e77846fd9aec39fa52740780ce4a1e9d783d
HorbynkoIvan <horbynko.i@gmail.com>
2018-09-02 19:00:29 +0300

    work with events

--- Tutorials/4_events/script.js
+++ Tutorials/4_events/script.js
@@ -1,37 +1,21 @@
 $(function () {
-    $('#content').css({
-        'color': 'red',
-        'margin-left': '20px',
-        'font-size': '12px'
-    })
-
-    // $('#content').css('height', function (value) {
-    //     return parseFloat(value)*2;
+    // $('header').find('a').on('click', function (event) {
+    //     alert("Hello");
+    //     event.preventDefault();
+    //     //предовращает срабатывание события на родителе во время всплытия события
+    //     event.stopPropagation();
+    //     //есть короткая запись для этих двух событий
+    //     // return false
+    //
+    // });
+    // $('header').on('click', function (event) {
+    //     alert("Hello header");
     // })
 
-    //add class to element
-    $('#content').addClass('name');
-
-    //check for a class
-    $('#content').hasClass('name');
-
-    //remove class
-    $('#content').removeClass('name');
-
-    //toggle for a class
-    $('#content').toggleClass('name');
-
-    //work with attr()
-    //find a[href]
-    var atr = $('header').find("a").attr('href')
-    console.log("attr" + atr)
-
-    var imgTitle = $('img').attr('title', "new image")
-    var imgSrc = $('img').attr('src', "/image/png/arrow.png")
-
-    $('#content').find('a').attr({
-        "href": 'https://www.youtube.com/watch?v=eFrtW9O7Rtk',
-        'title': "work with attr"
-    }).text(this.title)
+    $('#quantity').keyup(function () {
+        var value = $(this).val();
+        $('#message').text(value);
+        return false;
+    })
 
 });
